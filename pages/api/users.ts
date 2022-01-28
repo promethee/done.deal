462f688566d4247bdeb295224336ece4262e03bb
promethee <lunette.francois@gmail.com>
2022-01-28 22:28:28 +0100

    add RandomUser interface to api/user

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -3,7 +3,27 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 const pgp = pgPromise({ noWarnings: true })
 
-const db = pgp(`postgres:://User:Password@data.local:5432/users`)
+const db = pgp(`postgres://User:Password@data.local:5432/dealt`)
+
+export interface RandomUser {
+  name: {
+    title: string
+    first: string
+    last: string
+  }
+  pictures: {
+    [key: string]: string
+  }
+}
+
+export interface User {
+  id: string
+  title: string
+  firstname: string
+  lastname: string
+  thumbnail: string
+  picture: string
+}
 
 const handler = async (_: NextApiRequest, res: NextApiResponse) => {
   try {
